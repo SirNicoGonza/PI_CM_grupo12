56309d495259bab9754c91878fe895c4c78ce772
Ferco7 <bynanofer@hotmail.com>
2024-08-09 10:19:29 -0300

    Implementacion del buscador, solo busca por pagina actual

--- src/components/playlists/Playlists.jsx
+++ src/components/playlists/Playlists.jsx
@@ -2,12 +2,13 @@ import PlaylistsCard from "./PlaylistsCard";
 import useFetch from "../../hooks/useFetch";
 import React, { useEffect, useState } from 'react';
 
-// Componente que genera una lista de card con todas las playlists de la API
+// Componente de todas las playlists de la API
 function Playlists() {
     const [playlists, setPlaylists] = useState([]);
     const [currentPage, setCurrentPage] = useState(1); // Página actual
     const [totalPages, setTotalPages] = useState(0); // Total de páginas
-    const playlistsPerPage = 5; // Cantidad de playlists por página
+    const [searchTerm, setSearchTerm] = useState(''); // Término de búsqueda
+    const playlistsPerPage = 10; // Cantidad de playlists por página
 
     // Generar la URL de la API para la página actual
     const nextUrl = `${import.meta.env.VITE_API_BASE_URL_HARMONY}/playlists/?page=${currentPage}&page_size=${playlistsPerPage}`;
@@ -37,16 +38,29 @@ function Playlists() {
         }
     };
 
+    const filteredPlaylists = playlists.filter(playlist =>
+        playlist.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     if (isLoading && playlists.length === 0) return <p>Cargando...</p>;
     if (isError) return <p>Error al cargar las listas de reproducción</p>;
     if (playlists.length === 0) return <p>No hay listas de reproducción disponibles</p>;
 
     return (
-        <div>
+        <div style={{ paddingLeft: '20px', paddingRight: '20px' }}>
             <div className="my-5">
                 <h2 className="title">Lista de Reproducción</h2>
+                
+                <input
+                    type="text"
+                    placeholder="Buscar en Playlist ..."
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    style={{ marginBottom: '15px', marginLeft: '10px', padding: '5px', width: 'calc(30% - 20px)' }}
+                />
+                
                 <ul>
-                    {playlists.map(playlist => (
+                    {filteredPlaylists.map(playlist => (
                         <div key={playlist.id} className="column is-two-third">
                             <PlaylistsCard playlist={playlist} />
                         </div>
