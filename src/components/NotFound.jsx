c18c8fa1e42cf8ace45953b92e586236afc980b9
SirNicoGonza <marcosnicolasgonza@gmail.com>
2024-08-11 17:29:59 -0300

    Se estilizo NotFound

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,8 +1,11 @@
+import "../assets/Login.css";
+
 export default function NotFound(){
     return (
-        <div>
+        <div id="container-login">
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-            <h2>Pagina no encontrada</h2>
+            <h2 id="title">Pagina no encontrada</h2>
+            <p>ERROR 404</p>
             <i className="material-symbols-outlined">database_off</i>
         </div>
     )
