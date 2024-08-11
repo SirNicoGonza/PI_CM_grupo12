b2ceb51c7c3b97a28753aacb9a7b457ae719b025
Ferco7 <bynanofer@hotmail.com>
2024-08-10 22:36:28 -0300

    fix warning en navmenu 'activeclassName'

--- src/components/navbar/NavMenu.jsx
+++ src/components/navbar/NavMenu.jsx
@@ -1,28 +1,27 @@
-import { NavLink } from "react-router-dom";
-import "../navbar/NavMenu.css";
+import { NavLink } from 'react-router-dom';
+import '../navbar/NavMenu.css';
 
-function NavMenu({ items }){
-    return (
-        <div className="navbar-menu">
-            <div className="navbar-start">
-                {items.map((item, index) =>(
-                    <NavLink
-                        key={index}
-                        to={item.url}
-                        activeclassName={({ isActive, isPending, isTransitioning }) =>
-                            [
-                                isPending ? "pending" : "",
-                                isActive ? "active" : "",
-                                isTransitioning ? "transitioning" : "",
-                            ].join("navbar-item")
-                        }
-                    >
-                        {item.text}
-                    </NavLink>
-                ))}
-            </div>
-        </div>
-    )
-};
+function NavMenu({ items }) {
+	return (
+		<div className='navbar-menu'>
+			<div className='navbar-start'>
+				{items.map((item, index) => (
+					<NavLink
+						key={index}
+						to={item.url}
+						className={({ isActive, isPending }) =>
+							[
+								isPending ? 'pending' : '',
+								isActive ? 'active navbar-item' : 'navbar-item',
+							].join(' ')
+						}
+					>
+						{item.text}
+					</NavLink>
+				))}
+			</div>
+		</div>
+	);
+}
 
 export default NavMenu;
